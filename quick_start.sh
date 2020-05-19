914580518d8e9f180ac41768a037c2719da70fa5
tony <tony92151@gmail.com>
2020-05-19 15:19:22 +0800

    add some fun

--- quick_start.sh
+++ quick_start.sh
@@ -50,15 +50,37 @@ echo
 echo "Prepare..."
 sleep 1
 bash EOS-lab-testnet/start_node.sh init >> /dev/null 2>&1
+
+ip=$(hostname -I)
+ip=${ip% }:9876
+sed -i "s/p2p-peer-address = $ip//" node/config.ini
+
 bash EOS-lab-testnet/start_node.sh start >> nodeos.log 2>&1 &
 
-echo "Node is runing... "
+echo " ==================="
+echo "|Node is running... |"
+echo " ==================="
 echo "Log : $PWD/nodeos.log"
+
+echo "System account key :\n$eosio_prikey\n$eosio_pubkey" >> node.info
+echo "Node info save at : $PWD/node.info"
 
 sleep 1
 
-echo "System account key : \n $eosio_prikey \n $eosio_pubkey" >> nodeos.con
-echo "Node info save at : $PWD/nodeos.con"
+## Ordinary
+
+# if [ $nodetype == "ordinary" ]
+# then
+#     echo "Do you want to use default key-pair for system account ?"
+#     echo "Default private key : 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+#     select yn in "default" "custom"; do
+#         case $yn in
+#             "default" ) eosio_prikey=5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3; break;;
+#             "custom" ) read -p "Enter new private key: "  eosio_prikey && read -p "Enter new public key: "  eosio_pubkey; break;;
+#         esac
+#     done
+# fi 
+
 
 
 
