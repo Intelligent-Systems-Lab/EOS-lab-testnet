a6992baa26715b3da8abede9844c7e343569b0c6
tony <tony92151@gmail.com>
2020-06-17 21:21:20 +0800

    increase vote

--- eosvote.py
+++ eosvote.py
@@ -1,5 +1,5 @@
 import random 
-r = random.sample(range(1,25), 3)
+r = random.sample(range(1,25), 5)
 print('cleos -u http://localhost:8800 system voteproducer prods ${!user_name}',end=' ')
 for i in r :
     print('$'+'user'+str('{0:04}'.format(i))+'_name ',end=' ')
